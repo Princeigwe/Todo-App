7a159dd2d8a5cbf1b3da597bb44e17fa30c0a433
Prince Igwenagha <Igwep297@gmail.com>
2021-07-04 18:07:50 +0100

    made little changes

--- app/src/main/java/com/example/todo/TaskAdapter.kt
+++ app/src/main/java/com/example/todo/TaskAdapter.kt
@@ -4,6 +4,7 @@ import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.ItemTouchHelper
